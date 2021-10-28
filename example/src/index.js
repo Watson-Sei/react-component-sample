e55f8f69136f76d1d0efebef07f1a5b00258cb2f
Watson-Sei <seinabehack@gmail.com>
2021-10-28 15:11:13 +0900

    first commit

---
+++ example/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {Text} from '../../src/lib';
+
+const App = () => <Text />;
+render(<App />, document.getElementById('root'));
---
+++ src/lib/components/Text.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Text() {
+    return (
+        <h1>Hello MY name is Library</h1>
+    )
+}
+
+export default Text;
